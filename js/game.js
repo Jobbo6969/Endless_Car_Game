e9e8c9916086a10c59e78f9e959d98241325c171
HUMMEZ <33335749+hummez@users.noreply.github.com>
2017-11-03 13:13:10 +1100

    added some extra lines

--- js/game.js
+++ js/game.js
@@ -1,11 +1,13 @@
 window.onload = function() {
 
 //create a new phaser game, with dimensions of 800px and 600px tall
-var game = new phaser.game(800, 600, phaser.auto, '',{ preload: preload, create: create, updsate: update });
+var game = new phaser.game(800, 600, phaser.auto, '',{preload: preload, create: create, update: update });
 
 //preload function, where we can load all assets that will be used
-funtion create() {
-
+funtion preload() {
+  //game.load.image('nameInCode', 'assets/NameFolder.ext');
+  //game.load.image('backround', 'assets/namefolder.ext');
+  //game.load.image('ongoingtraffic', 'assets/namefolder.ext');
 }
 
 //create function, where all the inilial objects are created
@@ -13,7 +15,7 @@ function create() {
 
 }
 
-//this runsevery frame
+//this runs every frame
 function update() {
 
 }
