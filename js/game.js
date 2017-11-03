3981528a2c6fe0929cc98d6222a4b6540b262176
HUMMEZ <33335749+hummez@users.noreply.github.com>
2017-11-03 13:02:38 +1100

    add new code

--- js/game.js
+++ js/game.js
@@ -1,3 +1,20 @@
-window.onload.function();
+window.onload = function() {
 
-game.new.Phaser
+//create a new phaser game, with dimensions of 800px and 600px tall
+var game = new phaser.game(800, 600, phaser.auto, '',{ preload: preload, create: create, updsate: update });
+
+//preload function, where we can load all assets that will be used
+funtion create() {
+
+}
+
+//create function, where all the inilial objects are created
+function create() {
+
+}
+
+//this runsevery frame
+function update() {
+
+}
+};
